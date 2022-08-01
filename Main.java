a801c78c1731a81c511292c5e6049baa1b2d7bae
Andrew Lazarenkov <lazarenkovandrew@gmail.com>
2022-08-01 22:52:02 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int cost = 12554;
+        int miles = cost/20;
+        System.out.println("Стоимость билета - " + (cost) + " рублей.");
+        System.out.println("Начислено - " + (miles) + " миль.");
+    }
+
+}
