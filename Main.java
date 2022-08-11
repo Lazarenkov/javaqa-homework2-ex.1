397e9cb4da24cff08cef56ad570fbd5f79ba4976
Lazarenkov <108696283+Lazarenkov@users.noreply.github.com>
2022-08-11 23:53:43 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        int cost = 12554;
-        int miles = cost / 20;
-        System.out.println("Стоимость билета - " + (cost) + " рублей.");
-        System.out.println("Начислено - " + (miles) + " миль.");
-    }
-}
