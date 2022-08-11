0424031eef61a83ce0f5d27e8778e29bbe690304
Andrew Lazarenkov <lazarenkovandrew@gmail.com>
2022-08-11 23:32:01 +0300

    fix: project recreated

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 12554;
+        int miles = cost / 20;
+        System.out.println("Стоимость билета - " + (cost) + " рублей.");
+        System.out.println("Начислено - " + (miles) + " миль.");
+    }
+}
